0732d4be009a738a8b5b2680be510f3eb6e6b026
No-Face-the-3rd <foodflare@yahoo.com>
2016-08-25 14:27:53 -0700

    Shaders

--- RenderUtils/crenderutils.h
+++ RenderUtils/crenderutils.h
@@ -3,8 +3,29 @@
 #ifndef CRENDERUTILS_H
 #define CRENDERUTILS_H
 
-void test();
 
+
+struct Geometry
+{
+	// VBO : Vertex Buffer Object : an array of vertices
+	// IBO : Index Buffer Object : an array of indices(triangles)
+	// VAO : Vertex Array Object : Groups the two with some formatting
+	// size : number of triangles
+	unsigned vao, vbo, ibo, size;
+};
+
+Geometry makeGeometry(const struct Vertex *verts, size_t v_size, const unsigned int *tris, size_t tsize);
+
+void freeGeometry(Geometry &geo);
+
+struct Shader
+{
+	unsigned handle;
+};
+
+Shader makeShader(const char *vsource, const char *fsource);
+
+void freeShader(Shader &shader);
 
 
 #endif
