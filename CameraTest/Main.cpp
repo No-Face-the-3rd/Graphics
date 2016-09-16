533e746a545ff0aa425817d33c08b16fafb2f588
No-Face-the-3rd <foodflare@yahoo.com>
2016-09-15 17:46:39 -0700

    stuffzors

--- CameraTest/Main.cpp
+++ CameraTest/Main.cpp
@@ -7,7 +7,7 @@
 #include "camera.h"
 #include "procgen.h"
 
-
+#include "Vertex.h"
 #include "glm\glm.hpp"
 #include "glm\ext.hpp"
 
@@ -27,15 +27,15 @@ void main()
 	input.init(window);
 	timer.init();
 
-	gallery.generatePlane("meow", 64, 64);
+	//gallery.generatePlane("meow", 64, 64);
 
-	Geometry purple = genGrid(256, 75);
+	//Geometry purple = genGrid(256, 75);
 
 	unsigned char pixels[] ={125,125,0};
 	Texture tex2 = makeTexture(1, 1, 0x1907, pixels);
 	Texture tex = loadTexture("../res/textures/meow.jpg");
 	Texture tex3 = loadTexture("../res/textures/MewMap.jpg");
-	Texture tex4 = genNoise(256, 1);
+	//Texture tex4 = genNoise(256, 1);
 
 	gallery.loadShader("simple", "../res/shaders/simple.vs", "../res/shaders/simple.fs");
 	gallery.loadShader("camera", "../res/shaders/camera.vs", "../res/shaders/camera.fs");
@@ -45,7 +45,7 @@ void main()
 	gallery.loadObjectOBJ("cube", "../res/models/cube.obj");
 	gallery.loadObjectOBJ("sphere", "../res/models/sphere.obj");
 	gallery.loadObjectOBJ("spear", "../res/models/soulspear.obj");
-	gallery.loadObjectOBJ("bunny", "../res/models/Bunny.obj");
+	//gallery.loadObjectOBJ("bunny", "../res/models/Bunny.obj");
 
 	float ident[16] = { 1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 };
 
@@ -72,13 +72,25 @@ void main()
 	gallery.loadTexture("spearSpec", "../res/textures/soulspear_specular.tga");
 
 	Texture tray[] = {gallery.getTexture("spearDif"),gallery.getTexture("spearNorm"), gallery.getTexture("spearSpec")};
-	Texture atray[] = { tex4,tex4,tex4 };
+	//Texture atray[] = { tex4,tex4,tex4 };
+
+	frameBuffer frame = makeFrameBuffer(1360, 768, 2);
+	frameBuffer screen = { 0,1360,768,1 };
+
+	vertex veets[4] = { {{-1,-1,0,1},{},{},{0,0}},{{1,-1,0,1},{},{},{1,0}},{{1,1,0,1},{},{},{1,1}},{{-1,1,0,1},{},{},{0,1}} };
+	unsigned trees[] = { 0,1,2,2,3,0 };
+
+
+	gallery.makeGeometry("quee", veets, 4, trees, 6);
+
+	gallery.loadShader("post","../res/shaders/post.vs", "../res/shaders/post.fs");
+
 	while (window.step())
 	{
+		clearFrameBuffer(frame);
 		timer.step();
 		input.step();
 		
-
 		view = cam.getView();
 		proj = cam.getProjection();
 
@@ -90,7 +102,13 @@ void main()
 		mod = glm::rotate(curTime, glm::vec3(0.0f, 1.0f, 0.0f));
 
 		
-		draw(gallery.getShader("lighting"),  gallery.getGeometry("spear"), glm::value_ptr(mod), glm::value_ptr(view), glm::value_ptr(proj), tray, 3, curTime);
+		draw(gallery.getShader("lighting"), gallery.getGeometry("spear"), frame, glm::value_ptr(mod), glm::value_ptr(view), glm::value_ptr(proj), tray, 3, curTime);
+		draw(gallery.getShader("lighting"), gallery.getGeometry("spear"), frame, glm::value_ptr( glm::translate(glm::vec3(4.0f, 0.0f,0.0f)) * mod), glm::value_ptr(view), glm::value_ptr(proj), tray, 3, curTime);
+
+
+
+		draw(gallery.getShader("post"), gallery.getGeometry("quee"), screen, glm::value_ptr(glm::mat4()), glm::value_ptr(glm::mat4()), glm::value_ptr(glm::mat4()), frame.colors, frame.numColors, curTime);
+
 
 		//draw(gallery.getShader("lighting"), gallery.getGeometry("bunny"), glm::value_ptr(mod * glm::translate(glm::vec3(20.0f,5.0f,0.0f))), glm::value_ptr(view), glm::value_ptr(proj), tray, 3, curTime);
 
@@ -110,36 +128,42 @@ void main()
 		//draw(gallery.getShader("texture2"), gallery.getGeometry("cube"), tex, glm::value_ptr(mod), glm::value_ptr(view), glm::value_ptr(proj));
 
 		//draw(gallery.getShader("texture"), gallery.getGeometry("cube"), tex, glm::value_ptr(glm::rotate(curTime, glm::vec3(0.0f, fmod(curTime, 1.0f), 0.0f)) * model * glm::translate(glm::vec3(3.0f, 1.0f, 0.0f))), glm::value_ptr(view), glm::value_ptr(proj), curTime);
-		//float test = fabs(fmod(curTime / 6.0f, 6.0f));
-		//if (test < 1.0f)
+		/*float test = fabs(fmod(curTime / 6.0f, 6.0f));
+		if (test < 1.0f)
 		{
-			//moveX = -3.0f + fmod(curTime, 6.0f);
+			moveX = -3.0f + fmod(curTime, 6.0f);
 		}
-		//else if (test > 1.0f && test < 2.0f)
+		else if (test > 1.0f && test < 2.0f)
 		{
-			//moveY = -3.0f + fmod(curTime, 6.0f);
+			moveY = -3.0f + fmod(curTime, 6.0f);
 		}
-		//else if (test > 2.0f && test < 3.0f)
+		else if (test > 2.0f && test < 3.0f)
 		{
-			//moveZ = -3.0f + fmod(curTime, 6.0f);
+			moveZ = -3.0f + fmod(curTime, 6.0f);
 		}
-		//else if (test > 3.0f && test < 4.0f)
+		else if (test > 3.0f && test < 4.0f)
 		{
-			//moveX = 3.0f - fmod(curTime, 6.0f);
+			moveX = 3.0f - fmod(curTime, 6.0f);
 		}
-		//else if (test > 4.0f && test < 5.0f)
+		else if (test > 4.0f && test < 5.0f)
 		{
-			//moveY = 3.0f - fmod(curTime, 6.0f);
+			moveY = 3.0f - fmod(curTime, 6.0f);
 		}
-		//else
+		else
 		{
-			//moveZ = 4.0f - fmod(curTime, 10.0f);
+			moveZ = 4.0f - fmod(curTime, 10.0f);
 		}
-		//glm::mat4 modelo = glm::translate(glm::vec3(moveX, moveY, moveZ));
-		//draw(gallery.getShader("texture"), gallery.getGeometry("cube"),tex, glm::value_ptr(modelo), glm::value_ptr(view), glm::value_ptr(proj),curTime);
+		glm::mat4 modelo = glm::translate(glm::vec3(moveX, moveY, moveZ));
+		draw(gallery.getShader("texture"), gallery.getGeometry("cube"),tex, glm::value_ptr(modelo), glm::value_ptr(view), glm::value_ptr(proj),curTime);*/
 	}
 
+	freeFrameBuffer(frame);
+
+
 	freeTexture(tex);
+	freeTexture(tex2);
+	freeTexture(tex3);
+//	freeTexture(tex4);
 
 	input.term();
 	timer.term();
---
+++ res/shaders/post.vs
@@ -0,0 +1,45 @@
+// shadertype=glsl
+
+#version 430
+
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec4 color;
+layout(location = 2) in vec4 normal;
+layout(location = 3) in vec2 texCoord;
+
+out vec4 vPosition;
+out vec4 vColor;
+out vec4 vNormal;
+out vec2 UV;
+
+layout(location = 0) uniform mat4 proj;
+layout(location = 1) uniform mat4 view;
+layout(location = 2) uniform mat4 model;
+layout(location = 3) uniform float time;
+
+
+layout(location = 4) uniform vec4 lightDir;
+layout(location = 5) uniform vec4 lightAmb;
+layout(location = 6) uniform vec4 lightDif;
+layout(location = 7) uniform vec4 lightSpec;
+
+layout(location = 8) uniform vec4 surfAmb;
+layout(location = 9) uniform vec4 surfDif;
+layout(location = 10) uniform vec4 surfSpec;
+
+layout(location = 11) uniform float specPow;
+
+layout(location = 12) uniform sampler2D diffM;
+layout(location = 13) uniform sampler2D normalM;
+layout(location = 14) uniform sampler2D specM;
+
+
+void main()
+{
+
+
+
+	gl_Position = position;
+	UV = texCoord;
+
+}
